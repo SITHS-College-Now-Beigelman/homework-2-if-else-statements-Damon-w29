1c4f195e0983e5025e4618ac7a21654419ef063e
Damon-w29 <Damonw18@nycstudents.net>
2024-10-03 22:03:56 -0400

    adasdlkawjdfiopjqawfrj

--- Wu_Damon_HW_2_Part_2.cpp
+++ Wu_Damon_HW_2_Part_2.cpp
@@ -1,6 +1,6 @@
 // Damon Wu 
 // 10/2/2024
-// Hw 2 part 1 
+// Hw 2 part 2
 
 #include<iostream>
 #include<string>
@@ -9,7 +9,17 @@
 using namespace std;
 
 int main(){
+    double score;
     double judge1;
+    double judge2;
+    double judge3;
+    double judge4;
+    double judge5;
+    double judge6;
 
+    cout <<"Judge 1 please put your score in.";
+    cin >> judge1;
+
+    
 
 }
