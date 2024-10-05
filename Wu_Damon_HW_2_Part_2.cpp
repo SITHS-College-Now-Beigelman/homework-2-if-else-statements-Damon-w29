593a86ca70e2b3082a54fcb9f99c59f0d58fd010
Damon-w29 <Damonw18@nycstudents.net>
2024-10-04 22:52:44 -0400

    updates

--- Wu_Damon_HW_2_Part_2.cpp
+++ Wu_Damon_HW_2_Part_2.cpp
@@ -13,34 +13,37 @@ int main(){
 // Lotta doubles to mark down each persons score. I use a low and high to eliminate the outliers whilst scores 1-6 are used to take in inputs, I will then organize the scores. points 1 - 4  will be used to find the average.
     double high;
     double low;
+// takes in the inputs
     double score1;
     double score2;
     double score3;
     double score4;
     double score5;
     double score6;
+//inputs are then sorted into the points used for calculations
     double point1;
     double point2;
     double point3;
     double point4;
 
-
-    // I take the first input from judge 1, its then automatically sorted into the highest one since its the first one, then it proceeds.
+    //takes in the first point measurement, since its the first one, it will be nuetral till more inforation is gotten. 
     cout <<"Judge 1 please put your score in, remember, the max score is 10 and the lowest is 0, put your scores in whole numbers! \n";
     cin >> score1;
-    score1 = high;
+    score1 = point1;
 
-    // Now the big blocks slowly come in, this takes the input from judge 2 and then compares it to the other scores. 
+    // slighty more complex, if score 2 is greater than score 1, then it becomes the new high, if its lower, then point 1 becomes the new high and score 2 becomes the new low. 
     cout <<"Judge 2 please put your score in. \n";
     cin >> score2;
     if(score2 > score1){
         score2 = high;
-        score1 = low; 
+        score1 = low;
         }
-    else {
+        if (score1 > score2) {
         score2 = low;
+        score1 = high;
         }
     
+    // yay complexity, if score 3 is greater than the before mentioned high, then that becomes the new high, if its lower than the before mentioned low, it becomes the new low, if its neither, it becomes point1. 
     cout <<"Judge 3 please put your score in. \n";
     cin >> score3;
     if(score3 > high){
@@ -49,7 +52,7 @@ int main(){
         if(score3 < low){
             score3 = low;
         }
-        else{
+        else {
             score3 = point1;
         }
     
@@ -81,11 +84,6 @@ int main(){
         }
     
     cout << "Your score is " << (point1 + point2 + point3 + point4)/4 << " good job! \n";
-    
 
-        
-        
-
-
-
+    return 0; // ends code
 }
