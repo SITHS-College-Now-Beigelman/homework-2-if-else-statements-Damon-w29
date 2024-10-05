11eaf9b5a2cc6c5131ab82e7c361c701881fbfe7
Damon-w29 <Damonw18@nycstudents.net>
2024-10-04 22:08:40 -0400

    part 2

--- Wu_Damon_HW_2_Part_2.cpp
+++ Wu_Damon_HW_2_Part_2.cpp
@@ -9,17 +9,83 @@
 using namespace std;
 
 int main(){
-    double score;
-    double judge1;
-    double judge2;
-    double judge3;
-    double judge4;
-    double judge5;
-    double judge6;
 
-    cout <<"Judge 1 please put your score in.";
-    cin >> judge1;
+// Lotta doubles to mark down each persons score. I use a low and high to eliminate the outliers whilst scores 1-6 are used to take in inputs, I will then organize the scores. points 1 - 4  will be used to find the average.
+    double high;
+    double low;
+    double score1;
+    double score2;
+    double score3;
+    double score4;
+    double score5;
+    double score6;
+    double point1;
+    double point2;
+    double point3;
+    double point4;
 
+
+    // I take the first input from judge 1, its then automatically sorted into the highest one since its the first one, then it proceeds.
+    cout <<"Judge 1 please put your score in, remember, the max score is 10 and the lowest is 0, put your scores in whole numbers! \n";
+    cin >> score1;
+    score1 = high;
+
+    // Now the big blocks slowly come in, this takes the input from judge 2 and then compares it to the other scores. 
+    cout <<"Judge 2 please put your score in. \n";
+    cin >> score2;
+    if(score2 > score1){
+        score2 = high;
+        score1 = low; 
+        }
+    else {
+        score2 = low;
+        }
     
+    cout <<"Judge 3 please put your score in. \n";
+    cin >> score3;
+    if(score3 > high){
+        score3 = high;
+    }
+        if(score3 < low){
+            score3 = low;
+        }
+        else{
+            score3 = point1;
+        }
+    
+    cout << "Judge 4 please put your score in. \n";
+    cin >> score4;
+     if(score4 > high){
+        score4 = high;
+    }
+        if(score4 < low){
+            score4 = low;
+        }
+    
+    cout << "Judge 5 please put your score in. \n";
+    cin >> score5;
+     if(score5 > high){
+        score5 = high;
+    }
+        if(score5 < low){
+            score5 = low;
+        }
+
+    cout << "Judge 6 please put your score in. \n";
+    cin >> score6;
+     if(score6 > high){
+        score6 = high;
+    }
+        if(score6 < low){
+            score6 = low;
+        }
+    
+    cout << "Your score is " << (point1 + point2 + point3 + point4)/4 << " good job! \n";
+    
+
+        
+        
+
+
 
 }
---
+++ misstype/Wu_Damon_Lab_3_Part_1.cpp
@@ -0,0 +1,80 @@
+// Damon Wu 
+// 10/2/2024
+// Hw 2 part 1 
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    int month;
+    int day;
+
+    // user inputs month and day when prompted to 
+    cout << "To determine the current season, please input a month and date in numerical format, (for example, march first is 3, 1)  \n";
+    cin >> month;
+    cin >> day;
+    
+    // in case the user inputs a month over 12 or day over 31, outputs text that tells the user that something is wrong.
+    if(day > 31 || day < 1 || month > 13 || month < 1 )
+        cout << "Invalid date. Remember, there are 12 months in a year and up to 31 days in a month, if you put a negative, why?";
+
+    if(month == 1) {                             // if the month inputed is 1, it tells that its currently winter since there is no date of transition in the month of January. 
+        cout << "It is currently Winter.";
+    }
+    if(month == 2){                              // if the month inputed is 2, it tells that its currently winter since there is no date of transition in the month of Febuary.
+        cout << "It is currently Winter.";
+    }
+    if(month == 3){                              // if the month inputed is 3, and the day 20 or below, it prints winter, however if the day is at least 21 it prints spring due to 3/21 being the transition to spring. 
+       if(day <= 20){
+       cout << "It is currently Winter.";
+       }
+        else {
+        cout << "It is currently Spring.";
+        }
+    }
+    if(month == 4){                             // if the month inputed is 4, it tells that its currently Spring since there is no date of transition in the month of April.
+        cout << "It is currently Spring.";
+    }
+    if(month == 5){                             // if the month inputed is 5, it tells that its currently Spring since there is no date of transition in the month of May. 
+        cout << "It is currently Spring.";
+    }
+    if(month == 6){                             // if the month inputed is 6, and the day 20 or below, it prints Spring, however if the day is at least 21 it prints Summer due to 6/21 being the transition to Summer.
+        if(day <= 20){
+        cout << "It is currently Spring.";
+        }
+        else {
+            cout << "it is currently Summer.";
+        }
+    }
+    if(month == 7){                             // if the month inputed is 7, it tells that its currently Summer since there is no date of transition in the month of July.
+        cout << "It is currently Summer.";
+    }
+    if(month == 8){                             // if the month inputed is 8, it tells that its currently Summer since there is no date of transition in the month of August.
+        cout << "It is currently Summer.";
+    }
+    if(month == 9){                             // if the month inputed is 9, and the day 22 or below, it prints Summer, however if the day is at least 23 it prints Fall, due to 9/23 being the transition to Fall.
+        if(day <=22){
+        cout << "It is currently Summer.";
+        }
+        else {
+            cout << "It is currently Fall.";
+        }
+    }
+    if(month == 10){                            // if the month inputed is 10, it tells that its currently Fall since there is no date of transition in the month of October.
+        cout << "It is currently Fall.";
+    }
+    if(month == 11){                            // if the month inputed is 11, it tells that its currently Fall since there is no date of transition in the month of November.
+        cout << "It is currently Fall.";
+    }
+    if(month == 12){                            // if the month inputed is 12, and the day 21 or below, it prints Fall, however if the day is at least 22 it prints Winter, due to 12/22 being the transition to Winter.
+        if(day <= 21){
+        cout << "It is currently Fall.";
+        }
+        else {
+            cout << "it is currently Winter.";
+        }
+    return 0;   // ends the code 
+    }
+}
